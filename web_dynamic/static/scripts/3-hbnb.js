fd3ca2b484d9dfc7ff3d33ced3eb61bbf051eaa0
Matias Acosta <2277@holbertonschool.com>
2021-05-14 12:03:05 -0300

    Task 3

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -10,4 +10,12 @@ $('document').ready(function () {
       }
       $('.amenities H4').text(Object.values(amenities).join(', '));
     });
+    const url = 'http://0.0.0.0:5001/api/v1/status/';
+    $.get(url, function (response) {
+      if (response.status === 'OK') {
+        $('DIV#api_status').addClass('available');
+      } else {
+        $('DIV#api_status').removeClass('available');
+      }
+    });
   });
